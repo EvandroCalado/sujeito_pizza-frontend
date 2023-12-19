5494e4900d7eaf6cc17f01c6fe7b3ecce930a704
Evandro Calado <evandrocalado07@gmail.com>
2023-12-18 23:34:33 -0300

    ✨ feat(pages): create signup

--- src/pages/signup/index.tsx
+++ src/pages/signup/index.tsx
@@ -4,11 +4,11 @@ import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function Home() {
+export default function Signup() {
   return (
     <>
       <Head>
-        <title>Sujeito Pizza - Faça seu login</title>
+        <title>Faça seu cadastro agora!</title>
       </Head>
       <div className="flex min-h-screen flex-col items-center justify-center">
         <Image
@@ -21,16 +21,17 @@ export default function Home() {
 
         <div className="mt-8 flex w-[100%] flex-col items-center justify-center px-6 py-8 md:w-[600px]">
           <form className="flex w-[90%] flex-col">
+            <Input type="text" placeholder="Digite seu nome" />
             <Input type="text" placeholder="Digite seu email" />
             <Input type="password" placeholder="Digite sua senha" />
 
             <Button type="submit" loading={false}>
-              Acessar
+              Cadastrar
             </Button>
           </form>
 
-          <Link href={'/signup'} className="mt-4 cursor-pointer text-white">
-            Não possue uma conta? Cadastre-se!
+          <Link href={'/'} className="mt-4 cursor-pointer text-white">
+            Já possue conta? Faça login!
           </Link>
         </div>
       </div>
